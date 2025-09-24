39dd400653626ae5797fc070474c859b6dd384d0
Adityan S <official.adityan@gmail.com>
2025-09-24 17:15:14 +0530

    with action

--- build.el
+++ build.el
@@ -13,21 +13,34 @@
 
 (setq org-publish-project-alist
       (list
-       (list "my-org-site"
+       ;; This part converts your .org files to .html
+       (list "site-org"
              :recursive t
              :base-directory "./content"
              :publishing-directory "./public"
              :publishing-function 'org-html-publish-to-html
-	     :with-author nil           
+             :with-author nil
              :with-creator nil
              :with-toc t
-	     :section-numbers nil
-	     :time-stamp-file nil)))
+             :section-numbers nil
+             :time-stamp-file nil)
+
+       ;; NEW PART: This copies all other files (css, jpg, png, etc.)
+       (list "site-static"
+             :recursive t
+             :base-directory "./content"
+             :publishing-directory "./public"
+             :publishing-function 'org-publish-attachment
+             ;; Exclude Org files, which are handled by the first component
+             :exclude "\\.org$")
+
+       ;; This combines the two components above into a single project
+       (list "site" :components '("site-org" "site-static"))))
 
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil)
 
-(org-publish-all t)
+(org-publish "site" t)
 (message "Build complete!")
 
